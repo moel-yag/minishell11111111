039690405cdd45a813219e9db3ada018ca148477
moel-yag <moel-yag@student.1337.ma>
2025-08-04 10:50:15 +0100

    spliting the parser func. but not complited yet.

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -2,215 +2,115 @@
 
 static int	handle_word_token(t_ast *curr, t_token *current)
 {
-    char	*arg_copy;
+	char	*arg_copy;
 
-    arg_copy = ft_strdup(current->value);
-    ft_gc_add(arg_copy);
-    if (!arg_copy)
-        return (0);
-    if (!ft_lst_push(&curr->args, arg_copy))
-        return (0);
-    return (1);
+	arg_copy = ft_strdup(current->value);
+	ft_gc_add(arg_copy);
+	if (!arg_copy)
+		return (0);
+	if (!ft_lst_push(&curr->args, arg_copy))
+		return (0);
+	return (1);
 }
 
 static int	handle_redirection_token(t_ast *curr, t_token *current)
 {
-    t_redir	*redir;
+	t_redir	*redir;
 
-    if (current->next && current->next->type == TOKEN_WORD)
-    {
-        redir = ft_malloc(sizeof(t_redir));
-        if (!redir)
-            return (0);
-        redir->type = current->type;
-        redir->filename = ft_strdup(current->next->value);
-        ft_gc_add(redir->filename);
-        if (!redir->filename)
-            return (0);
-        if (!ft_lst_push(&curr->redirections, redir))
-            return (0);
-        return (2); // signal to skip next token
-    }
-    return (0);
+	if (current->next && current->next->type == TOKEN_WORD)
+	{
+		redir = ft_malloc(sizeof(t_redir));
+		if (!redir)
+			return (0);
+		redir->type = current->type;
+		redir->filename = ft_strdup(current->next->value);
+		ft_gc_add(redir->filename);
+		if (!redir->filename)
+			return (0);
+		if (!ft_lst_push(&curr->redirections, redir))
+			return (0);
+		return (2);
+	}
+	return (0);
 }
 
 static int	handle_pipe_token(t_ast **ast, t_ast **curr, t_token *current)
 {
-    t_ast	*new_node;
+	t_ast	*new_node;
 
-    if (current->next->type != TOKEN_WORD
-        && !ft_token_is_redirection(current->next->type))
-    {
-        ft_putstr_fd("Syntax error: Pipe not followed by a command or\
-            redirection.\n", 2);
-        return (0);
-    }
-    new_node = create_ast_node();
-    if (!new_node)
-    {
-        ft_putstr_fd("Error creating new AST node for pipe.\n", 2);
-        return (0);
-    }
-    add_ast_node(ast, new_node);
-    *curr = new_node;
-    return (1);
+	if (current->next->type != TOKEN_WORD
+		&& !ft_token_is_redirection(current->next->type))
+	{
+		ft_putstr_fd("Syntax error: Pipe not followed by a command or\
+			redirection.\n", 2);
+		return (0);
+	}
+	new_node = create_ast_node();
+	if (!new_node)
+	{
+		ft_putstr_fd("Error creating new AST node for pipe.\n", 2);
+		return (0);
+	}
+	add_ast_node(ast, new_node);
+	*curr = new_node;
+	return (1);
 }
 
 t_ast	*parser(const char *input)
 {
-    t_token	*tokens;
-    t_token	*current;
-    t_ast	*ast;
-    t_ast	*curr;
-    int		ret;
+	t_token	*tokens;
+	t_token	*current;
+	t_ast	*ast;
+	t_ast	*curr;
+	int		ret;
 
-    tokens = tokenize(input);
-    current = tokens;
-    if (!tokens)
-    {
-        ft_putstr_fd("Error tokenizing input.\n", 2);
-        return (NULL);
-    }
-    expand(g_data.env_list, tokens);
-    ast = NULL;
-    curr = create_ast_node();
-    ast = curr;
-    if (!curr)
-    {
-        ft_putstr_fd("Error creating AST node.\n", 2);
-        return (NULL);
-    }
-    while (current)
-    {
-        if (current->type == TOKEN_EOF)
-            break ;
-        else if (current->type == TOKEN_WORD)
-        {
-            if (!handle_word_token(curr, current))
-                return (free_tokens(tokens), NULL);
-        }
-        else if (ft_token_is_redirection(current->type))
-        {
-            ret = handle_redirection_token(curr, current);
-            if (ret == 0)
-                return (free_tokens(tokens), NULL);
-            if (ret == 2)
-                current = current->next;
-        }
-        else if (current->type == TOKEN_PIPE)
-        {
-            if (!handle_pipe_token(&ast, &curr, current))
-                return (free_tokens(tokens), NULL);
-        }
-        else
-        {
-            ft_putstr_fd("Unexpected token type: ", 2);
-            ft_putnbr_fd(current->type, 2);
-            ft_putstr_fd("\n", 2);
-            return (free_tokens(tokens), NULL);
-        }
-        current = current->next;
-    }
-    free_tokens(tokens);
-    return (ast);
+	tokens = tokenize(input);
+	current = tokens;
+	if (!tokens)
+	{
+		ft_putstr_fd("Error tokenizing input.\n", 2);
+		return (NULL);
+	}
+	expand(g_data.env_list, tokens);
+	ast = NULL;
+	curr = create_ast_node();
+	ast = curr;
+	if (!curr)
+	{
+		ft_putstr_fd("Error creating AST node.\n", 2);
+		return (NULL);
+	}
+	while (current)
+	{
+		if (current->type == TOKEN_EOF)
+			break ;
+		else if (current->type == TOKEN_WORD)
+		{
+			if (!handle_word_token(curr, current))
+				return (free_tokens(tokens), NULL);
+		}
+		else if (ft_token_is_redirection(current->type))
+		{
+			ret = handle_redirection_token(curr, current);
+			if (ret == 0)
+				return (free_tokens(tokens), NULL);
+			if (ret == 2)
+				current = current->next;
+		}
+		else if (current->type == TOKEN_PIPE)
+		{
+			if (!handle_pipe_token(&ast, &curr, current))
+				return (free_tokens(tokens), NULL);
+		}
+		else
+		{
+			ft_putstr_fd("Unexpected token type: ", 2);
+			ft_putnbr_fd(current->type, 2);
+			ft_putstr_fd("\n", 2);
+			return (free_tokens(tokens), NULL);
+		}
+		current = current->next;
+	}
+	free_tokens(tokens);
+	return (ast);
 }
-
-
-
-
-
-
-// t_ast	*parser(const char *input)
-// {
-// 	t_token	*tokens;
-// 	t_token	*current;
-// 	t_token	*print_token;
-// 	t_ast	*ast;
-// 	t_ast	*curr;
-// 	char	*arg_copy;
-// 	t_redir	*redir;
-// 	t_ast	*new_node;
-
-// 	tokens = tokenize(input);
-// 	current = tokens;
-// 	if (!tokens)
-// 	{
-// 		ft_putstr_fd("Error tokenizing input.\n", 2);
-// 		return (NULL);
-// 	}
-// 	expand(g_data.env_list, tokens);
-// 	print_token = tokens;
-// 	while (print_token && print_token->type != TOKEN_EOF)
-// 		print_token = print_token->next;
-// 	ast = NULL;
-// 	curr = create_ast_node();
-// 	ast = curr;
-// 	if (!curr)
-// 	{
-// 		ft_putstr_fd("Error creating AST node.\n", 2);
-// 		return (NULL);
-// 	}
-// 	while (current)
-// 	{
-// 		if (current->type == TOKEN_EOF)
-// 			break ;
-// 		else if (current->type == TOKEN_WORD)
-// 		{
-// 			arg_copy = ft_strdup(current->value);
-// 			ft_gc_add(arg_copy);
-// 			if (!arg_copy)
-// 			{
-// 				return (NULL);
-// 			}
-// 			if (!ft_lst_push(&curr->args, arg_copy))
-// 				return (NULL);
-// 		}
-// 		else if (ft_token_is_redirection(current->type))
-// 		{
-// 			if (current->next && current->next->type == TOKEN_WORD)
-// 			{
-// 				redir = ft_malloc(sizeof(t_redir));
-// 				if (!redir)
-// 					return (NULL);
-// 				redir->type = current->type;
-// 				redir->filename = ft_strdup(current->next->value);
-// 				ft_gc_add(redir->filename);
-// 				if (!redir->filename)
-// 					return (NULL);
-// 				if (!ft_lst_push(&curr->redirections, redir))
-// 					return (NULL);
-// 				current = current->next;
-// 			}
-// 			else
-// 				return (NULL);
-// 		}
-// 		else if (current->type == TOKEN_PIPE)
-// 		{
-// 			if (current->next->type != TOKEN_WORD
-// 				&& !ft_token_is_redirection(current->next->type))
-// 			{
-// 				ft_putstr_fd("Syntax error: Pipe not followed by a command or\
-// 					redirection.\n", 2);
-// 				return (NULL);
-// 			}
-// 			new_node = create_ast_node();
-// 			if (!new_node)
-// 			{
-// 				ft_putstr_fd("Error creating new AST node for pipe.\n", 2);
-// 				return (NULL);
-// 			}
-// 			add_ast_node(&ast, new_node);
-// 			curr = new_node;
-// 		}
-// 		else
-// 		{
-// 			ft_putstr_fd("Unexpected token type: ", 2);
-// 			ft_putnbr_fd(current->type, 2);
-// 			ft_putstr_fd("\n", 2);
-// 			return (NULL);
-// 		}
-// 		current = current->next;
-// 	}
-// 	free_tokens(tokens);
-// 	return (ast);
-// }
